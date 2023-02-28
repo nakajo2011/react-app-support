ad534fa3d79ac8ae888f112c4dfd24ba70f21879
Yukishige Nakajo <nakajo@chaintope.com>
2023-02-28 12:22:39 +0900

    スライドのアニメーションにSwiperを使うように変更した。

--- src/components/BasicCard.js
+++ src/components/BasicCard.js
@@ -1,127 +1,129 @@
 // 他のモジュールから取り込む
 import * as React from 'react';
-import  { createContext, useState } from 'react';
+import {createContext, useState} from 'react';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
-import { Slide } from '@mui/material';
 
-import CardBody from './CardBody'
+import {Swiper, SwiperSlide} from 'swiper/react';
+import {Navigation} from 'swiper';
+import 'swiper/css';
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
 
 // Contextっていうグローバル変数みたいなものを使って本文をstring型の配列で定義
 export const BodyText = createContext()
 
 //　関数の呼び出し
 export default function BasicCard(props) {
-  // const,letで指定。varはあまり使わない
-  const { classes } = props;
-  // カウント定義
-  const [count, setCount] = useState(0);
-  const [checked, setChecked] = React.useState(true);
-  // タイトル
-  const [text, setText] = useState(["新しいページ"])
-  
-  // 渡された値をコンソールに表示する、どんな型でもOK
-  console.log(text);
+    // const,letで指定。varはあまり使わない
+    const {classes} = props;
+    // 最初に表示するスライド定義
+    const [initialIndex, setInitialIndex] = useState(0);
+    // タイトル
+    const [text, setText] = useState(["新しいページ"])
+    // ナビゲーション
+    const swiperRef = React.useRef();
 
-  // 左の三角ボタンを押すと次々変わる
-  function handleClick(e) {
-    // TとFを反転
-    const newChecked = !checked
-    setChecked(newChecked);
+    // 渡された値をコンソールに表示する、どんな型でもOK
+    console.log(text);
 
-    // 押す度に前のページへ
-    let newCount = count-1
-    // 値を表示
-    console.log(newCount)
-    // カウントが０以下であれば最後の記事に戻る
-    if(newCount < 0) {
-      newCount = text.length-1
+    // 左の三角ボタンを押すと次々変わる
+    function handleClick(e) {
+        // Swiper Navigationを使って1つ前のスライドを表示
+        swiperRef.current?.slidePrev()
     }
-    // 次のステートを直接引数で受け取るインターフェイス
-    setCount(newCount)
-  }
 
-  function randomClick(e){
-    let size = text.length
-    var index = Math.floor(Math.random()*size)
-    while (index === count && size > 1) {
-      index = Math.floor(Math.random()*size)
+    function randomClick(e) {
+        let size = text.length
+        var index = Math.floor(Math.random() * size)
+        while (index === swiperRef.current?.activeIndex && size > 1) {
+            index = Math.floor(Math.random() * size)
+        }
+        // 今作ったページを表示するようにする
+        // TODO: 最後のページの1つ前しか表示されない。SwiperSlideがまだ増えてないのが原因と思われる。
+        swiperRef.current?.slideTo(index)
     }
-    console.log(index)
-    setCount(index)
-   }
 
-  // 右の三角をクリックすると次のページに移る
-  function handleRightClick(e) {
-    // 押す度に前のページへ
-    let newCount = count+1
-    // 値を表示
-    console.log(newCount)
-    // カウントが記事数以上であれば０の値に戻る
-    if(newCount >= text.length) {
-      newCount = 0
+    // 右の三角をクリックすると次のページに移る
+    function handleRightClick(e) {
+        // Swiper Navigationを使って次のスライドを表示
+        swiperRef.current?.slideNext()
     }
-    // 次のステートを直接引数で受け取るインターフェイス
-    setCount(newCount)
-  }
 
-  // 新しい本文を作成
-  function createNewText(e) {
-    console.log(text)
+    // 新しい本文を作成
+    function createNewText(e) {
+        console.log(text)
 
-    const len = text.length
-    // 一番後ろに新しいページを作成
-    setText([...text, `新しいページ${len}`])
-    // 今作ったページを表示するようにする
-    setCount(text.length-1)
-  }
+        const len = text.length
+        // 一番後ろに新しいページを作成
+        setText([...text, `新しいページ${len}`])
+        // 今作ったページを表示するようにする
+        swiperRef.current?.slideTo(text.length)
+    }
 
-  return (
-    <Box
-    xs={{ width: '60rem' }}>
-    <Grid
-      container 
-      spacing={0}
-      // direction="column"
-      alignItems="center"
-      justifyContent="center"
-      align="center"
-      
-      // style={{ minHeight: '100vh' }}
-      sx={{ p: 1 }}
-    >
-      
-      <Grid item xs={2} sx={{mb: 55}}>
-        <img src="./img/sea10hennkou.png" alt="my image" width="85px" />
-      </Grid>
-      <Slide direction="up" in={checked} mountOnEnter unmountOnExit> 
-        <Grid item xs={8} sx={{mb: 10}}>
-          <BodyText.Provider value={{text, setText}}>
-            <CardBody index={count}/>
-          </BodyText.Provider>
-        </Grid>
-      </Slide>
-      <Slide direction="up" in={!checked} mountOnEnter unmountOnExit> 
-        <Grid item xs={8} sx={{mb: 10}}>
-          <BodyText.Provider value={{text, setText}}>
-            <CardBody index={count}/>
-          </BodyText.Provider>
-        </Grid>
-      </Slide>
-      <Grid item xs={2} sx={{mb: 40}}>
-        <img src="./img/plus.png" alt="my image" width="88px" onClick={createNewText}/>
-      </Grid>
-      <Grid item  xs={4}>
-        <img src="./img/left.png" alt="my image" width="80px" onClick={handleClick}/>
-      </Grid>
-      <Grid item xs={4}>
-        <img src="./img/sh.png" alt="my image" width="160px" onClick={randomClick} />
-      </Grid>
-      <Grid item xs={4}>
-        <img src="./img/right.png" alt="my image" width="86px" onClick={handleRightClick}/>
+    function getSlideList() {
+        const list = []
+        text.forEach((t) => {
+            list.push(
+                <SwiperSlide>
+                    <Card sx={{ m: 1, height: "30em", justifyContent: "center", alignItems: "center", display:"flex"}}>
+                        <CardContent id="MyCardContent" >
+                            <Typography sx={{ fontSize: 20 }} color="text.secondary" gutterBottom>
+                                {t}
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                </SwiperSlide>
+            )
+        })
+        return list
+    }
 
-      </Grid>
-    </Grid>
-    </Box>
-  )
+    return (
+        <Box
+            xs={{width: '60rem'}}>
+            <Grid
+                container
+                spacing={0}
+                // direction="column"
+                alignItems="center"
+                justifyContent="center"
+                align="center"
+
+                // style={{ minHeight: '100vh' }}
+                sx={{p: 1}}
+            >
+
+                <Grid item xs={2} sx={{mb: 55}}>
+                    <img src="./img/sea10hennkou.png" alt="my image" width="85px"/>
+                </Grid>
+                <Grid item xs={8} sx={{mb: 10}}>
+                    <BodyText.Provider value={{text, setText}}>
+                        <Swiper
+                            modules={[Navigation]}
+                            onBeforeInit={(swiper) => {
+                                swiperRef.current = swiper;
+                            }}
+                        >
+                            {getSlideList()}
+                        </Swiper>
+                    </BodyText.Provider>
+                </Grid>
+                <Grid item xs={2} sx={{mb: 40}}>
+                    <img src="./img/plus.png" alt="my image" width="88px" onClick={createNewText}/>
+                </Grid>
+                <Grid item xs={4}>
+                    <img src="./img/left.png" alt="my image" width="80px" onClick={handleClick}/>
+                </Grid>
+                <Grid item xs={4}>
+                    <img src="./img/sh.png" alt="my image" width="160px" onClick={randomClick}/>
+                </Grid>
+                <Grid item xs={4}>
+                    <img src="./img/right.png" alt="my image" width="86px" onClick={handleRightClick}/>
+
+                </Grid>
+            </Grid>
+        </Box>
+    )
 }
